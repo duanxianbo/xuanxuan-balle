97b526593895296efdf3f9841a712af8c511ee24
xianbo duan <duanxianbo@gmail.com>
2020-07-04 17:54:58 -0400

    fix(pdf_contract): improve layout

--- functions/addContract/index.js
+++ functions/addContract/index.js
@@ -15,49 +15,52 @@ exports.main = (event, context) => {
     var doc = new PDFDocument();
 
     doc.font(`${__dirname}/msyh.ttf`)
+    .lineGap(10);
     doc.fontSize(25).text(contractTtile, {align: "center"});
-    doc.fontSize(15).text("甲方（受教育方）：学生姓名", 0, undefined);
+    doc.fontSize(12).text("甲方（受教育方）：学生姓名", 80);
     doc.moveUp();
-    doc.text(name, 200, undefined, {underline: true})
+    doc.text(name, 280, undefined, {underline: true})
     doc.moveUp();
-    doc.text(";性别", 260)
+    doc.text(";性别", 330)
     doc.moveUp();
-    doc.text(sex, 300,  undefined, {underline: true})
+    doc.text(sex, 370,  undefined, {underline: true})
     doc.moveUp();
-    doc.text(";出生年月 ", 320)
+    doc.text(";出生年月 ", 390)
     doc.moveUp();
-    doc.text(birth, 390, undefined, {underline: true});
+    doc.text(birth, 460, undefined, {underline: true});
     doc.moveUp();
-    doc.text(";", 470);
-    doc.text("甲方监护人 : 姓名", 0);
+    doc.text(";", 540);
+    doc.moveDown();
+    doc.text("甲方监护人 : 姓名", 80);
     doc.moveUp();
-    doc.text(parentsName, 120, undefined, {underline: true});
+    doc.text(parentsName, 210, undefined, {underline: true});
     doc.moveUp();
-    doc.text(";身份证号码 ", 180)
+    doc.text(";身份证号码 ", 270)
     doc.moveUp();
-    doc.text(parentsIdcard, 260, undefined, {underline: true})
+    doc.text(parentsIdcard, 360, undefined, {underline: true})
     doc.moveUp();
-    doc.text(";", 350);
-    doc.text("乙方 ", 0);
+    doc.text(";", 540);
+    doc.moveDown();
+    doc.text("乙方 ", 80);
     doc.moveUp();
-    doc.text(companyName, 30, undefined, {underline: true})
-    doc.text(contractRules, 0);
+    doc.text(companyName, 110, undefined, {underline: true})
+    doc.text(contractRules, 80);
+    doc.text()
     doc.moveUp();
-    doc.text(classNumber, 70, undefined, {underline: true});
+    doc.text(classNumber, 150, undefined, {underline: true});
     doc.moveUp();
-    doc.text("课时（45分钟/课时)", 110);
-    doc.text("3、课程费用总计：", 0);
+    doc.text("课时（45分钟/课时)",180);
+    doc.text("3、课程费用总计：", 80);
     doc.moveUp();
-    doc.text(totalPrice, 120, undefined, {underline: true});
+    doc.text(totalPrice, 210, undefined, {underline: true});
     doc.moveUp();
-    doc.text("元。", 180);
+    doc.text("元。", 260);
 
-    doc.text(otherContractRules,  0)
+    doc.text(otherContractRules,  80);
+    doc.moveDown(2);
+    doc.fontSize(20).text(rulesTitle, {align: "center"});
 
-    doc.addPage();
-    doc.fontSize(25).text(rulesTitle, {align: "center"});
-
-    doc.fontSize(15).text(detailRules, { align: "justify"});
+    doc.fontSize(12).text(detailRules, { align: "justify"});
 
     const buffers = [];
     doc.on('data', buffers.push.bind(buffers));
