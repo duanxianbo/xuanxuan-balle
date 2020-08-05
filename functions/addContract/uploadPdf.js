9a5d9341283e3000d22315ef6e9a0807286b5753
xianbo duan <duanxianbo@gmail.com>
2020-08-05 09:23:26 -0400

    fix(pdf): contract update

--- functions/addContract/uploadPdf.js
+++ functions/addContract/uploadPdf.js
@@ -1,6 +1,8 @@
 const PDFDocument = require('pdfkit');
 const {detailRules, otherContractRules, contractRules,  companyName,
-  rulesTitle,
+  rulesTitle,    contractRuleOneTitle,
+  contractRuleOneContent,
+  signaturePositions,
   contractTtile} = require('./config');
 const now = new Date();
 
@@ -13,55 +15,58 @@ exports.uploadPdf = (event, contractName, cloud) => {
 
   return new Promise((resolve, reject) => {
     const doc = new PDFDocument();
+    doc.registerFont('Heading Font', `${__dirname}/Alibaba-PuHuiTi-Heavy.ttf`);
+    doc.registerFont('Normal Font', `${__dirname}/Alibaba-PuHuiTi-Regular.ttf`);
+    doc.font("Heading Font")
+    .lineGap(10).fontSize(15).text(contractTtile, {align: "center"});
+    
+    doc.fontSize(12).font("Normal Font").text("甲方（受教育方）：学生姓名 ", {continued: true})
+    .text(name, {underline: true, continued: true})
+    .text("; 性别 ", {continued: true})
+    .text(sex, {underline: true, continued: true})
+    .text("; 出生年月 ", {continued: true})
+    .text(birth, {underline: true, continued: true})
+    .text(";");
+   
+    doc.text("甲方监护人 : 姓名 ", {continued: true})
+    .text(parentsName, {underline: true, continued: true})
+    .text("; 身份证号码 ", {continued: true})
+    .text(parentsIdCard, {underline: true, continued: true})
+    .text(";");
+  
+    doc.text("乙方 ", {continued: true})
+    .text(companyName, {underline: true})
+    
+    doc.text(contractRules);
 
-    doc.font(`${__dirname}/msyh.ttf`)
-    .lineGap(10);
-    doc.fontSize(25).text(contractTtile, {align: "center"});
-    doc.fontSize(12).text("甲方（受教育方）：学生姓名", 80);
-    doc.moveUp();
-    doc.text(name, 280, undefined, {underline: true})
-    doc.moveUp();
-    doc.text(";性别", 330)
-    doc.moveUp();
-    doc.text(sex, 370,  undefined, {underline: true})
-    doc.moveUp();
-    doc.text(";出生年月 ", 390)
-    doc.moveUp();
-    doc.text(birth, 460, undefined, {underline: true});
-    doc.moveUp();
-    doc.text(";", 540);
-    doc.moveDown();
-    doc.text("甲方监护人 : 姓名", 80);
-    doc.moveUp();
-    doc.text(parentsName, 210, undefined, {underline: true});
-    doc.moveUp();
-    doc.text(";身份证号码 ", 270)
-    doc.moveUp();
-    doc.text(parentsIdCard, 360, undefined, {underline: true})
-    doc.moveUp();
-    doc.text(";", 540);
-    doc.moveDown();
-    doc.text("乙方 ", 80);
-    doc.moveUp();
-    doc.text(companyName, 110, undefined, {underline: true})
-    doc.text(contractRules, 80);
-    doc.text()
-    doc.moveUp();
-    doc.text(classNumber, 150, undefined, {underline: true});
-    doc.moveUp();
-    doc.text("课时（45分钟/课时)",200);
-    doc.text("3、课程费用总计：", 80);
-    doc.moveUp();
-    doc.text(totalPrice, 180, undefined, {underline: true});
-    doc.moveUp();
-    doc.text("元。", 260);
+    doc.font('Heading Font').text(contractRuleOneTitle);
+    doc.font('Normal Font').text(contractRuleOneContent, {continued: true})
+    .text(classNumber, {underline: true, continued: true})
+    .text("课时（45分钟/课时)");
+    
+    doc.text("3、课程费用总计：", {continued: true})
+    .text(totalPrice,{underline: true, continued: true})
+    .text("元。");
 
-    doc.text(otherContractRules,  80);
+    otherContractRules.forEach((contractRule) => {
+      doc.font('Heading Font').text(contractRule.title);
+      doc.font('Normal Font').text(contractRule.content);
+    })
+  
+
     doc.moveDown(2);
-    doc.fontSize(20).text(rulesTitle, {align: "center"});
+    doc.fontSize(15).font('Heading Font').text(rulesTitle, {align: "center"});
 
-    doc.fontSize(12).text(detailRules, { align: "justify"});
-    doc.text(`日期 ${year} 年 ${month} 月 ${date} 日                                                    日期 ${year} 年 ${month} 月 ${date} 日`);
+    doc.fontSize(12);
+
+    detailRules.forEach((contractRule) => {
+      doc.font('Heading Font').text(contractRule.title);
+      doc.font('Normal Font').text(contractRule.content);
+    })
+
+    doc.text(signaturePositions);
+
+    doc.text(`日期 ${year} 年 ${month} 月 ${date} 日                                                                  日期 ${year} 年 ${month} 月 ${date} 日`);
 
     const buffers = [];
     doc.on('data', buffers.push.bind(buffers));
